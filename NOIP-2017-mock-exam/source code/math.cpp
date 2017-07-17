33d453a8d0cce06613cedd1ae281f9e4b8c05333
bufhdy <bufhdy@hotmail.com>
2017-07-17 18:55:55 +0800

    update NOIP mock exam

---
+++ NOIP-2017-mock-exam/source code/math.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <stack>
+using namespace std;
+const int MAXN = 100005;
+
+int main(void)
+{
+#ifndef LOCAL
+	freopen("math.in", "r", stdin);
+	freopen("math.out", "w", stdout);
+#endif // LOCAL
+
+	int k, p;
+	cin >> k >> p;
+
+
+	return 0;
+}

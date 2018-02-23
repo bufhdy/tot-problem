22354146f5fba265ddba595ac25193f0439cc6b2
bufhdy <bufhdy@hotmail.com>
2018-02-23 09:32:07 +0800

    Update Mul()

--- materials/maths theory/pow.cpp
+++ materials/maths theory/pow.cpp
@@ -1,4 +1,6 @@
 inline int Pow(long long a, int b)
+// 1 <= a, b, MOD <= 10^9
+// O(log b)
 {
 	long long Ans = 1;
 	while (b) {
@@ -6,6 +8,22 @@ inline int Pow(long long a, int b)
 			Ans = Ans * a % MOD;
 		
 		a = a * a % MOD;
+		b >>= 1;
+	}
+	
+	return Ans;
+}
+
+inline long long Mul(long long a, long long b)
+// 1 <= a, b, MOD <= 10^18
+// O(log b)
+{
+	long long Ans = 0;
+	while (b) {
+		if (b & 1)
+			Ans = (Ans + a) % MOD;
+		
+		a = a * 2 % MOD;
 		b >>= 1;
 	}
 	
